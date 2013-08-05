ba7f52f565fe0b367a7186a27df7dd244e39f0a8
Vitoria.Vasconcelos <ammai7@gmail.com>
2013-08-04 23:39:18 -0300

    Now saving Metas at database! =)

--- gen/com/project/meta/R.java
+++ gen/com/project/meta/R.java
@@ -27,10 +27,10 @@ public final class R {
         public static final int action_settings=0x7f08000a;
         public static final int btnAdd=0x7f080000;
         public static final int btnList=0x7f080001;
-        public static final int button1=0x7f080006;
-        public static final int editText1=0x7f080003;
-        public static final int editText2=0x7f080005;
+        public static final int descriptionField=0x7f080005;
         public static final int lv_metas=0x7f080009;
+        public static final int nameField=0x7f080003;
+        public static final int saveButton=0x7f080006;
         public static final int textView1=0x7f080002;
         public static final int textView2=0x7f080004;
         public static final int text_description=0x7f080008;
--- src/com/project/meta/ui/AddMetaActivity.java
+++ src/com/project/meta/ui/AddMetaActivity.java
@@ -1,14 +1,56 @@
 package com.project.meta.ui;
 
-import com.project.meta.R;
-
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.project.meta.R;
+import com.project.meta.model.Meta;
+import com.project.meta.repository.MetaRepository;
 
 public class AddMetaActivity extends Activity{
+	Meta meta;
+	
+	private Button saveButton;
+	private EditText name, description;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.add_meta);
+		
+		name = (EditText) findViewById(R.id.nameField);
+        description = (EditText) findViewById(R.id.descriptionField);
+        saveButton = (Button) findViewById(R.id.saveButton);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+					handleBtnClick();
+				}
+			});
+
 	}
+	
+    public void handleBtnClick() throws NullPointerException {
+    	
+    	meta = new Meta(name.getText().toString(), description.getText().toString());
+    	
+    	if(!meta.getName().isEmpty()) {
+       		MetaRepository.getInstance(AddMetaActivity.this).addMeta(meta);
+    		Toast.makeText(this, "New Meta saved!", Toast.LENGTH_LONG).show();
+    		
+    		Intent intent = new Intent(this, ListMetasActivity.class);
+    		startActivity(intent);
+    		
+    		name.setText("");
+    		description.setText("");
+    	} else {
+    		Toast.makeText(this, "Meta name can't be blank!", Toast.LENGTH_LONG).show();
+    	}
+    }
 }
