afd87c91b6b1cd1cf70507b7f66eed654abaccd8
Deise <infodeise@gmail.com>
2013-08-06 01:56:08 -0300

    configuracoes e chave gerada para o maps

--- gen/com/project/meta/R.java
+++ gen/com/project/meta/R.java
@@ -29,10 +29,10 @@ public final class R {
         public static final int btnList=0x7f080001;
         public static final int descriptionField=0x7f080005;
         public static final int lv_metas=0x7f080009;
-        public static final int nameField=0x7f080003;
+        public static final int nameField=0x7f080004;
         public static final int saveButton=0x7f080006;
         public static final int textView1=0x7f080002;
-        public static final int textView2=0x7f080004;
+        public static final int textView2=0x7f080003;
         public static final int text_description=0x7f080008;
         public static final int text_name=0x7f080007;
     }
